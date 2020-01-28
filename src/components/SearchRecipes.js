94bed9452280b1a23b9525e0dbaeb7344cb62aac
Saloni <sharmasaloni061@gmail.com>
2020-01-28 21:19:10 +0530

    Accessing the API

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -1,22 +1,36 @@
 import React, { Component } from 'react';
-import {Form, FormGroup, FormControl, FormLabel, Button} from 'react-bootstrap';
+import { Form, FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap';
 
 class SearchRecipes extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            ingredients: '',
+            dish: ''
+        }
+    }
+    search() {
+        let {ingredients, dish} = this.state;
+        const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+        console.log('state',this.state, 'url', url);
+    }
+
     render() {
         return (
             <Form inline>
                 <FormGroup>
                     <FormLabel>Ingredients</FormLabel>
-                    <FormControl type="text" placeholder="garlic, chicken" />
+                    <FormControl onChange={event => this.setState({ ingredients: event.target.value })} type="text" placeholder="garlic, chicken" />
                 </FormGroup>
                 {' '}
                 <FormGroup>
                     <FormLabel>Dish</FormLabel>
                     {' '}
-                    <FormControl type="text" placeholder="adobo" ></FormControl>
+                    <FormControl onChange={event => this.setState({ dish: event.target.value })} type="text" placeholder="adobo" ></FormControl>
                 </FormGroup>
                 {' '}
-                <Button>Submit</Button>
+                <Button onClick={()=>this.search()}>Submit</Button>
 
             </Form>
         )
