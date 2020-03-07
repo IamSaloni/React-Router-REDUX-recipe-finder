a6f2a2ace8b5bf8aa45210c5bbd164a66630d9a7
Saloni Sharma <sharmasaloni061@gmail.com>
2020-03-07 16:33:41 +0530

    accessing API and storing recipes

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -1,40 +1,61 @@
-import React, { Component } from 'react';
-import { Form, FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  FormControl,
+  FormLabel,
+  Button
+} from "react-bootstrap";
+import { connect } from "react-redux";
+import { setRecipes } from "../actions";
 
 class SearchRecipes extends Component {
-    constructor() {
-        super();
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            ingredients: '',
-            dish: ''
-        }
-    }
-    search() {
-        let {ingredients, dish} = this.state;
-        const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
-        console.log('state',this.state, 'url', url);
-    }
+    this.state = {
+      ingredients: "",
+      dish: ""
+    };
+  }
+  search() {
+    let { ingredients, dish } = this.state;
+    const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
 
-    render() {
-        return (
-            <Form inline>
-                <FormGroup>
-                    <FormLabel>Ingredients</FormLabel>
-                    <FormControl onChange={event => this.setState({ ingredients: event.target.value })} type="text" placeholder="garlic, chicken" />
-                </FormGroup>
-                {' '}
-                <FormGroup>
-                    <FormLabel>Dish</FormLabel>
-                    {' '}
-                    <FormControl onChange={event => this.setState({ dish: event.target.value })} type="text" placeholder="adobo" ></FormControl>
-                </FormGroup>
-                {' '}
-                <Button onClick={()=>this.search()}>Submit</Button>
+    fetch(url, {
+      method: "GET"
+    })
+      .then(response => response.json())
+      .then(json => {
+          this.props.setRecipes(json.results);
+      });
+  }
 
-            </Form>
-        )
-    }
+  render() {
+    return (
+      <Form inline>
+        <FormGroup>
+          <FormLabel>Ingredients</FormLabel>
+          <FormControl
+            onChange={event =>
+              this.setState({ ingredients: event.target.value })
+            }
+            type="text"
+            placeholder="garlic, chicken"
+          />
+        </FormGroup>{" "}
+        <FormGroup>
+          <FormLabel>Dish</FormLabel>{" "}
+          <FormControl
+            onChange={event => this.setState({ dish: event.target.value })}
+            type="text"
+            placeholder="adobo"
+          ></FormControl>
+        </FormGroup>{" "}
+        <Button onClick={() => this.search()}>Submit</Button>
+      </Form>
+    );
+  }
 }
 
-export default SearchRecipes;
+export default connect(null,{setRecipes})(SearchRecipes);
