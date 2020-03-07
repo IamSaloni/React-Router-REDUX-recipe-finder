7cbd32d7d4fdbb885eb64ba27c727a0ea07cd55f
Saloni Sharma <sharmasaloni061@gmail.com>
2020-03-07 22:09:16 +0530

    Storing Recipe Items

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { favouriteRecipe } from "../actions";
 
 class RecipeItem extends Component {
   render() {
     let { recipe } = this.props;
     return (
       <div className="recipe-item">
+        <div
+          className="star"
+          onClick={() => this.props.favouriteRecipe(recipe)}
+        >
+          &#9734;
+        </div>
         <div className="recipe-text">
           <a href={recipe.href}>
             <h4>{recipe.title}</h4>
@@ -17,4 +25,4 @@ class RecipeItem extends Component {
   }
 }
 
-export default RecipeItem;
+export default connect(null, { favouriteRecipe })(RecipeItem);
