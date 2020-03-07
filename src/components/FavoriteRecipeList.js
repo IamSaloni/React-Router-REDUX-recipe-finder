8baaa39622d42694d74bb5e8ee22596d9d2c9904
Saloni Sharma <sharmasaloni061@gmail.com>
2020-03-07 23:21:05 +0530

    react-router

--- src/components/FavoriteRecipeList.js
+++ src/components/FavoriteRecipeList.js
@@ -1,30 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import RecipeItem from './RecipeItem';
+import { Link } from "react-router-dom";
+import RecipeItem from "./RecipeItem";
 
 class FavoriteRecipeList extends Component {
-    render() {
-        return (
-            <div>
-                <h4>Favourite Recipes:</h4>
-                {
-                    this.props.favouriteRecipes.map((recipe,index)=>{
-                return (
-                    <RecipeItem key={index} recipe={recipe} favoriteButton={false} />
-                )
-            })
-                }
-                
-            </div>
-            
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h4 className="link">
+          <Link to="/">Home</Link>
+        </h4>
+        <h4>Favourite Recipes:</h4>
+        {this.props.favouriteRecipes.map((recipe, index) => {
+          return (
+            <RecipeItem key={index} recipe={recipe} favoriteButton={false} />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        favouriteRecipes: state.favouriteRecipes
-    }
+  return {
+    favouriteRecipes: state.favouriteRecipes
+  };
 }
 
-export default connect(mapStateToProps,null)(FavoriteRecipeList);
+export default connect(mapStateToProps, null)(FavoriteRecipeList);
