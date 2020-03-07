7d9b60187769ec12c94d80eed20f60c4530ab9bf
Saloni Sharma <sharmasaloni061@gmail.com>
2020-03-07 19:35:50 +0530

    Recipe List

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,18 @@
-import React, {Component} from 'react';
-import SearchRecipes from './SearchRecipes';
-import '../styles/index.css';
+import React, { Component } from "react";
+import SearchRecipes from "./SearchRecipes";
+import RecipeList from "./RecipeList";
+import "../styles/index.css";
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Recipe Finder</h2>
-                <SearchRecipes />
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <h2>Recipe Finder</h2>
+        <SearchRecipes />
+        <RecipeList />
+      </div>
+    );
+  }
 }
 
 export default App;
